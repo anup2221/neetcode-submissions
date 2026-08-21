810791a959548f0e031d81857cf8718bcf91a37a
anup2221 <80762398+anup2221@users.noreply.github.com>
2026-08-21 20:10:05 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> v;
+        v.push_back(nums[0]);
+        if(nums.size()==1)
+        return nums[0];
+        v.push_back(max(nums[0],nums[1]));
+        for(int i=2;i<nums.size();i++){
+            v.push_back(max(nums[i]+v[i-2],v[i-1]));
+        }
+        int ans=0;
+        for (int i=0;i<v.size();i++){
+            if(ans<v[i])
+            ans=v[i];
+        }
+        int n=v.size();
+        return v[n-1];
+        
+    }
+};
